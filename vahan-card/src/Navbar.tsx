415333c8cc5c15fdc0359eeb8115ed09ac88004d
Suyog Bhandari <suyog.bha.suyog1219@gmail.com>
2025-08-23 19:16:29 +0545

    navbar styling

--- vahan-card/src/Navbar.tsx
+++ vahan-card/src/Navbar.tsx
@@ -1,5 +1,16 @@
+import { useState } from "react";
+
 
 function Navbar(){
+    const [navOpen, setNavOpen] = useState(false);
+    const navClose = ()=>{
+        setNavOpen(false);
+    }
+
+    const navToggle=()=>{
+        setNavOpen(!navOpen);
+    }
+
     return(
         <>
         <header className="header">
@@ -19,7 +30,7 @@ function Navbar(){
                 <button className="btn btn-primary">Vahan Card for web</button>
             </div>
 
-            <div className="hamburger">
+            <div className="hamburger" onClick={navToggle}>
                 <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"
@@ -36,7 +47,41 @@ function Navbar(){
           <line x1="3" y1="18" x2="21" y2="18" />
         </svg>
             </div>
-        </header>       
+        </header>   
+
+
+        <div className={`side-nav ${navOpen ? "open":""}`}>
+            <div className="cross-icon" onClick={navClose}>
+                <svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="white"
+  strokeWidth="2"
+  strokeLinecap="round"
+  strokeLinejoin="round"
+>
+  <line x1="18" y1="6" x2="6" y2="18" />
+  <line x1="6" y1="6" x2="18" y2="18" />
+</svg>
+
+            </div>
+            <nav className="nav-mobile">
+                <ul className="mobile-list" >
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">How it works</a></li>
+                    <li><a href="/">Plans & Pricing</a></li>
+                    <li><a href="/">My Account</a></li>
+                    <li><a href="/">Get a Card</a></li>
+                </ul>
+            </nav>
+            <div className="mobile-buttons">
+                <button className="btn btn-secondary mobile-secondary"> <img src="./assets/warning-icon.png" alt="" className="btn-icon" /> Scam Alert</button>
+                <button className="btn btn-primary">Vahan Card for web</button>
+            </div>
+        </div>    
         </>
     )
 }
