5d82256b2641e04b7b2957350094a52a39778260
Suyog Bhandari <suyog.bha.suyog1219@gmail.com>
2025-08-23 18:14:37 +0545

    navbar-styling

--- vahan-card/src/Navbar.tsx
+++ vahan-card/src/Navbar.tsx
@@ -1,3 +1,4 @@
+
 function Navbar(){
     return(
         <>
@@ -16,6 +17,24 @@ function Navbar(){
             <div className="buttons">
                 <button className="btn btn-secondary"> <img src="./assets/warning-icon.png" alt="" className="btn-icon" /> Scam Alert</button>
                 <button className="btn btn-primary">Vahan Card for web</button>
+            </div>
+
+            <div className="hamburger">
+                <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="white"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <line x1="3" y1="6" x2="21" y2="6" />
+          <line x1="3" y1="12" x2="21" y2="12" />
+          <line x1="3" y1="18" x2="21" y2="18" />
+        </svg>
             </div>
         </header>       
         </>
