500d85da90e7f09ae0b8e0fa519b086bfbed4781
Suyog Bhandari <suyog.bha.suyog1219@gmail.com>
2025-08-23 11:29:41 +0545

    navbar-styling

--- vahan-card/src/Navbar.tsx
+++ vahan-card/src/Navbar.tsx
@@ -1,10 +1,10 @@
 function Navbar(){
     return(
         <>
-        <header className="header">
-            <img src="./assets/logo.png" alt="Logo" className="logo" />
+        <header className="header flex  items-center">
+            <img src="src/assets/logo.png" alt="Logo" className="logo" />
             <nav className="nav-links">
-                <ul>
+                <ul className="flex flex-row gap-8 items-center" >
                     <li><a href="/">Home</a></li>
                     <li><a href="/">How it works</a></li>
                     <li><a href="/">Plans & Pricing</a></li>
@@ -13,7 +13,7 @@ function Navbar(){
                 </ul>
             </nav>
 
-            <div className="buttons">
+            <div className="buttons ml-auto">
                 <button className="btn"> <img src="./assets/warning-icon.png" alt="" className="btn-icon" /> Scam Alert</button>
                 <button className="btn">Vahan Card for web</button>
             </div>
