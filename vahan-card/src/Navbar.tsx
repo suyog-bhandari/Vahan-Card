40b4983d31674410777198786f73c4a228c77c03
Suyog Bhandari <suyog.bha.suyog1219@gmail.com>
2025-08-23 13:13:01 +0545

    css changes

--- vahan-card/src/Navbar.tsx
+++ vahan-card/src/Navbar.tsx
@@ -1,19 +1,19 @@
 function Navbar(){
     return(
         <>
-        <header className="flex items-center">
+        <header className="header">
             <img src="src/assets/logo.png" alt="Logo" className="logo" />
-            <nav>
-                <ul className="flex flex-row items-center list-none gap-8" >
-                    <li><a href="/" className="no-underline">Home</a></li>
-                    <li><a href="/" className="no-underline">How it works</a></li>
-                    <li><a href="/" className="no-underline">Plans & Pricing</a></li>
-                    <li><a href="/" className="no-underline">My Account</a></li>
-                    <li><a href="/" className="no-underline">Get a Card</a></li>
+            <nav className="nav">
+                <ul className="nav-list" >
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">How it works</a></li>
+                    <li><a href="/">Plans & Pricing</a></li>
+                    <li><a href="/">My Account</a></li>
+                    <li><a href="/">Get a Card</a></li>
                 </ul>
             </nav>
 
-            <div className="buttons ml-auto">
+            <div className="buttons">
                 <button className="btn"> <img src="./assets/warning-icon.png" alt="" className="btn-icon" /> Scam Alert</button>
                 <button className="btn">Vahan Card for web</button>
             </div>
