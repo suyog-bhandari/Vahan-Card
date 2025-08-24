92b38f2da164af30f6ccf9918329f9700fa2267f
Suyog Bhandari <suyog.bha.suyog1219@gmail.com>
2025-08-24 11:03:17 +0545

    what we do section

--- vahan-card/src/Home.tsx
+++ vahan-card/src/Home.tsx
@@ -1,7 +1,26 @@
+import Card from "./Components.jsx"
+
 function Home(){
+    const cardsData=[
+        {title:"Enhance Convenience",
+        description:"Offer a hassle-free way to manage travel expenses using the Bahan Card.",
+        image:"/src/assets/payment-method.png"},
+
+        {title:"Save More",
+        description:"Enjoy exclusive student discounts and affordable fares every time you travel.",
+        image:"/src/assets/payment-method.png"},
+
+        {title:"Seamless Payments",
+        description:"Pay instantly with just a tap, no need to carry cash or worry about change.",
+        image:"/src/assets/payment-method.png"},
+
+        {title:"Track Expenses",
+        description:"Monitor your travel spending easily with clear and transparent transaction history.",
+        image:"/src/assets/payment-method.png"},
+
+    ]
     return(
         <main>
-            
             <section className="hero">
                 <div className="container">
                     <div className="inner-container">
@@ -19,6 +38,19 @@ function Home(){
             <section className="what-we-do">
                 <div className="container">
                     <h2>What we do</h2>
+
+                    <div className="cards-section">
+                        {
+                            cardsData.map((card, index)=>(
+                                <Card 
+                                key={index}
+                                title={card.title}
+                                description={card.description}
+                                image={card.image}
+                                />
+                            ))
+                        }
+                    </div>
                 </div>
             </section>
             
